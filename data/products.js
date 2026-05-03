e95652be255b6053450383dd45cf02ab91114f6f
Bezy427 <rusamubezel@gmail.com>
2026-05-04 01:54:19 +0200

    Learn more details about classes and this

--- data/products.js
+++ data/products.js
@@ -38,7 +38,7 @@ constructor(productDetails) {
   }
 
   extraInfoHTML() {
-    super.extraInfoHTML();
+    //super.extraInfoHTML();
     return '';
   }
 }
@@ -54,10 +54,35 @@ class Clothing extends Product {
   extraInfoHTML() {
     return `
       <a href="${this.sizeChartLink}" target="_blank">Size chart</a>
-    `
+    `;
   }
 
 }
+
+// const date = new Date();
+// console.log(date);
+// console.log(date.toLocaleTimeString());
+
+// console.log(this);
+
+// const object2 = {
+//   a: 2,
+//   b: this.a
+// }
+
+// function logThis() {
+//   console.log(this);
+// }
+// logThis();
+// logThis.call('hello');
+
+// this
+// const object3 = {
+//   method: () => {
+//     console.log(this);
+//   }
+// };
+// object3.method();
 
 export const products = [
   {
