aef77650997b48962ea9d4cded98ff0efa4ff8c5
Bezy427 <rusamubezel@gmail.com>
2026-05-04 16:41:41 +0200

    Convert from promises to async wait

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -5,27 +5,41 @@ import {loadCart} from "../data/cart.js";
 // import '../data/cart-class.js';
 // import '../data/backend-practice.js'
 
-Promise.all([
-  loadProductsFetch(),
-  new Promise((resolve) => {
-    loadCart(() => {
-      resolve();
-    });
-  })
-]).then((values) => {
-  console.log(values);
-  renderOrderSummary();
-  renderPaymentSummary();
-})
+async function loadPage() {
+  await loadProductsFetch();
 
-new Promise((resolve) => {
-  loadProducts(() => {
-    resolve();
+  const value = await new Promise((resolve) => {
+    loadCart(() => {
+      resolve('value3');
+    });
   });
-}).then(() => {
+
   renderOrderSummary();
   renderPaymentSummary();
-})
+}
+loadPage();
+
+// Promise.all([
+//   loadProductsFetch(),
+//   new Promise((resolve) => {
+//     loadCart(() => {
+//       resolve();
+//     });
+//   })
+// ]).then((values) => {
+//   console.log(values);
+//   renderOrderSummary();
+//   renderPaymentSummary();
+// });
+
+// new Promise((resolve) => {
+//   loadProducts(() => {
+//     resolve();
+//   });
+// }).then(() => {
+//   renderOrderSummary();
+//   renderPaymentSummary();
+// })
 
 /*
 loadProducts(() => {
